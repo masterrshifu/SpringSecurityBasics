523c25a9d5dd6954bb49127497302eee1a4f0090
Akshay Agarwal <105814323+masterrshifu@users.noreply.github.com>
2023-10-17 18:32:49 +0530

    initial commit

---
+++ Spring-Security-Project/src/main/java/com/springsecurity/controller/ProductController.java
@@ -0,0 +1,41 @@
+package com.springsecurity.controller;
+
+import com.springsecurity.dto.Product;
+import com.springsecurity.entity.UserInfo;
+import com.springsecurity.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+
+    @PostMapping("/new")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return service.addUser(userInfo);
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public List<Product> getAllTheProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public Product getProductById(@PathVariable int id) {
+        return service.getProduct(id);
+    }
+}
